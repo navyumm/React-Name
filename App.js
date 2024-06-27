5cfef5e27f2bdf6294f42b33ffae13a17f5e38df
navyumm <navyum12@gmail.com>
2024-06-27 16:50:48 +0530

    "Added README.md file with React Native introduction, course outline, and basic concepts"

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Button, Text, View } from 'react-native';
+import CompanyData from './components/CompanyData';
 
 
-let age=18;
-var email="abc@test.com"
+let age = 18;
+var email = "abc@test.com"
 
-function fruit(){
+function fruit() {
   return "apple";
 }
 
@@ -13,15 +14,23 @@ const App = () => {
 
   return (
     <View>
-      <Text style={{fontSize:30}}>hello</Text>
-      <Button title='press here'></Button>
-
-      <Text style={{fontSize:30}}>{age}</Text>
-      <Text style={{fontSize:30}}>{email}</Text>
-      <Text style={{fontSize:30}}>{fruit()}</Text>
-      <Text style={{fontSize:30}}>{age===18? "you are 18":"you are not 18"}</Text>
+      <UserData />
+      <Text style={{ fontSize: 30, fontWeight:'800' }}>Components</Text>
+      <CompanyData />
     </View>
   );
 }
+
+const UserData = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>Name : Navi</Text>
+      <Text style={{ fontSize: 30 }}>Age : 24</Text>
+      <Text style={{ fontSize: 30 }}>Email : test@google.com</Text>
+    </View>
+  )
+}
+
+
 
 export default App
---
+++ components/CompanyData.js
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native"
+
+const CompanyData = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>Name : xyz</Text>
+      <Text style={{ fontSize: 30 }}>Total Employee : 2400</Text>
+      <Text style={{ fontSize: 30 }}>Email : test@google.com</Text>
+    </View>
+  )
+}
+
+export default CompanyData
